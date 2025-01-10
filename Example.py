42c5317f0daa01a194f1df2ad67296966e25a585
Minh <quangtran102005@gmail.com>
2025-01-10 08:48:53 +0700

    package prepare

--- Example.py
+++ Example.py
@@ -1,7 +1,7 @@
 # Example usage
-import Camogen as cmg
+import Pycamo as pycamo
 # get color from image
-color_palette = cmg.extract_palette("demo_input/teste3.png", num_colors=4)
+color_palette = pycamo.extract_palette("demo_input/teste3.png", num_colors=4)
 
 # custom color palette
 #color_palette = ['0d011c', '1D1107', '011c07', '012e04']
@@ -10,4 +10,4 @@ pixelize = False # Set to True to pixelize the output image
 
 ratios = [25, 25, 25, 25]  # Example ratios for each color
 
-cmg.generate_pattern(color_palette, "gencamo.png", size=(1024, 1024), c=1.2, ratios=ratios, pixelize=pixelize)
+pycamo.generate_pattern(color_palette, "gencamo.png", size=(1024, 1024), c=1.2, ratios=ratios, pixelize=pixelize)
